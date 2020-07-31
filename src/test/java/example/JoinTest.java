f49f3cdce4c8d66d56cd11bf897b04cb07e90108
Bela Bagameri <bagameri.bela@gmail.com>
2020-08-01 01:23:18 +0200

    Neo4J 4.1 function example

--- src/test/java/example/JoinTest.java
+++ src/test/java/example/JoinTest.java
@@ -1,45 +1,27 @@
 package example;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.Config;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Session;
+import org.neo4j.harness.junit.rule.Neo4jRule;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.neo4j.driver.v1.Config;
-import org.neo4j.driver.v1.Driver;
-import org.neo4j.driver.v1.GraphDatabase;
-import org.neo4j.driver.v1.Session;
-import org.neo4j.harness.ServerControls;
-import org.neo4j.harness.TestServerBuilders;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class JoinTest {
 
-    private static final Config driverConfig = Config.build().withoutEncryption().toConfig();
-    private ServerControls embeddedDatabaseServer;
-
-    @BeforeAll
-    void initializeNeo4j() {
-
-        this.embeddedDatabaseServer = TestServerBuilders
-                .newInProcessBuilder()
-                .withFunction(Join.class)
-                .newServer();
-    }
+    @Rule
+    public Neo4jRule neo4j = new Neo4jRule().withFunction(Join.class);
 
     @Test
-    public void shouldAllowIndexingAndFindingANode() {
-
-        // This is in a try-block, to make sure we close the driver after the test
-        try( Driver driver = GraphDatabase.driver(embeddedDatabaseServer.boltURI(), driverConfig);
-             Session session = driver.session()) {
-
-            // When
-            String result = session.run( "RETURN example.join(['Hello', 'World']) AS result").single().get("result").asString();
-
-            // Then
-            assertThat( result).isEqualTo(( "Hello,World" ));
+    public void shouldJoinStrings() throws Throwable
+    {
+        try (Driver driver = GraphDatabase.driver(neo4j.boltURI(), Config.builder().withoutEncryption().build())) {
+            Session session = driver.session();
+            String result = session.run("RETURN example.join(['Hello', 'World']) AS result").single().get("result").asString();
+            assertEquals(result, "Hello,World");
         }
     }
 }
